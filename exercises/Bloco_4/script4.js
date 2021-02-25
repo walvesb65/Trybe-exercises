8fa594546650e67f81fc070903a0c4d7d5457bac
walvesb65 <willian.alvesb65@gmail.com>
2021-02-25 10:57:07 -0300

    Questao 4 da atividade

---
+++ exercises/Bloco_4/script4.js
@@ -0,0 +1,15 @@
+/* 4 - Faça um programa que, dado um valor definido numa variável, 
+retorne "positive" se esse valor for positivo,"negative" se for 
+negativo e "zero" caso contrário. */
+
+let v1 = ('positivo');
+let v2 = ('negativo');
+let v3 = 0;
+
+if(v1 == 'positivo'){
+  console.log(true);
+}else if(v2 == 'negativo'){
+  console.log(false);
+}else{
+  console.log (v3);
+}
