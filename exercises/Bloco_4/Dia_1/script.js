20c309265941b749760b75cda52a1fa21b7015f0
walvesb65 <willian.alvesb65@gmail.com>
2021-02-24 23:13:58 -0300

    atividade js questao 1 a 3

---
+++ exercises/Bloco_4/Dia_1/script.js
@@ -0,0 +1,33 @@
+let a = 8;
+let b = 5;
+
+let soma = a + b;
+let subtracao = a - b;
+let divisao = a / b;
+let multiplicacao = a * b;
+let modulo = a % b;
+
+/* console.log(soma, subtracao, divisao, multiplicacao, modulo) */
+
+/* Questão 2 */
+let num1 = 8;
+let num2 = 5;
+
+ if(num1 > num2){
+ /*  console.log(num1) */
+}else{
+  /* console.log(num2) */
+} 
+
+/* Questão 3 */
+let num3 = 8;
+let num4 = 5;
+let num5 = 29;
+
+if(num3 > num4 && num3 > num5){
+  /* console.log(num3) */
+}else if(num4 > num3 && num4 > num5){
+  /* console.log(num4) */
+}else{
+ /*  console.log(num5) */
+}
---
+++ exercises/Bloco_4/Dia_1/scripts.js
@@ -0,0 +1,36 @@
+/*Questão 1 
+
+const a = 10
+const b = 20
+
+const adicao = a + b;
+const subtracao = a - b;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = a % b;
+
+ console.log (adicao, subtracao, multiplicacao, divisao, modulo) 
+
+ Questão 2 
+
+let num1 = 10
+let num2 = 20
+
+if (num1 > num2){
+  console.log(num1)
+}else{
+  console.log(num2)
+}
+
+/* Questão 3 */
+let num3 = 10;
+let num4 = 5;
+let num5 = 30;
+
+if(num3 > num4 && num3 > num5){
+  console.log(num3)
+}else if(num4 > num3 && num4 > num5){
+  console.log(num4)
+}else{
+  console.log(num5)
+}
