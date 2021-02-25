9460cecba6be2ca505ee003ba2b040b303ad3c44
walvesb65 <willian.alvesb65@gmail.com>
2021-02-25 10:32:02 -0300

    Questao 2 da atividade

---
+++ exercises/Bloco_4/script2.js
@@ -0,0 +1,13 @@
+/* 2 - Faça um programa que retorne o maior de dois números. 
+Defina no começo do programa duas variáveis com os valores 
+que serão comparados. */
+
+let valor1 = 2;
+let valor2 = 3;
+
+if(valor1 > valor2){
+  console.log('valor1 é o maior');
+}else{
+  (valor2 > valor1)
+  console.log('valor2 é o maior');
+}
