e3c6aca93040d711225f7d54f56ca8a5dec02636
walvesb65 <willian.alvesb65@gmail.com>
2021-02-25 11:10:05 -0300

    Questao 5 da atividade

---
+++ exercises/Bloco_4/script5.js
@@ -0,0 +1,15 @@
+/* 5 - Faça um programa que defina três variáveis com os valores
+dos três ângulos internos de um triângulo. Retorne true se os ângulos
+representarem os ângulos de um triângulo e false caso contrário. */
+
+let ang1 = 40;
+let ang2 = 40;
+let ang3 = 100;
+
+let somatri = (ang1 + ang2 + ang3)
+
+if (somatri == 180){
+  console.log(true)
+}else{
+  console.log(false)
+}
