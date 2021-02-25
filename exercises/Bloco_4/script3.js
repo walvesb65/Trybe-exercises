60450212ed1d19e4a1a31dca3c332cdb140950d0
walvesb65 <willian.alvesb65@gmail.com>
2021-02-25 10:42:28 -0300

    Questao 3 da atividade

---
+++ exercises/Bloco_4/script3.js
@@ -0,0 +1,15 @@
+/* 3 -Faça um programa que retorne o maior de três números.
+ Defina no começo do programa três variáveis com os valores 
+ que serão comparados. */
+
+ let n1 = 100;
+ let n2 = 40;
+ let n3 = 30;
+
+ if(n1 > n2 && n3) {
+   console.log('n1 é o mairo');
+ }else if (n2 > n3 && n1){
+   console.log('n2 é o maior')
+ }else{
+   console.log('n3 é o maior')
+ }
