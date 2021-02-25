4d62713452be71d07766b7574f92254d7c207f70
walvesb65 <willian.alvesb65@gmail.com>
2021-02-25 01:10:24 -0300

    questao 7

--- exercises/Bloco_4/Dia_1/script.js
+++ exercises/Bloco_4/Dia_1/script.js
@@ -59,7 +59,7 @@ if(numero > 20){
    console.log(false);
  };
 
-  Questão 6 */
+  Questão 6 
 
 let peca = 'torre';
 
@@ -86,3 +86,25 @@ switch(peca){
     console.log('nenhuma peça valida');
 }
 
+ Questão 7 */
+
+ let nota = 110;
+
+ if(nota>=0 && nota <=100){
+ if(nota >= 90){
+    console.log('A')
+  }else if (nota >=80){
+    console.log('B')
+  }else if (nota >= 70){
+    console.log ('C')
+  }else if (nota >= 60){
+    console.log('D')
+  }else if (nota >= 50){
+    console.log('E')
+  }else{
+    console.log('F')
+  }
+}else{
+  console.log('nosta invalida')
+}
+  
