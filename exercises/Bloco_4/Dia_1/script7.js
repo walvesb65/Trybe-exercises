a209d46df40cff20bc9e031a68127090973eeceb
walvesb65 <willian.alvesb65@gmail.com>
2021-02-27 18:58:24 -0300

    corrigindo questao

--- exercises/Bloco_4/Dia_1/script7.js
+++ exercises/Bloco_4/Dia_1/script7.js
@@ -9,22 +9,21 @@ Porcentagem < 50 -> F
 O programa deve retornar uma mensagem de erro e encerrar 
 se a nota passada for menor que 0 ou maior que 100. */
 
-let nota = 79
+let nota = 49
 
-if (nota >=0 && nota <= 100){ 
-if (nota >= 90){
-  console.log('A')
-}else if (nota >= 80){
-  console.log('B')
-}else if (nota >= 70){
-  console.log ('C')
-}else if (nota >= 60){
-  console.log('D')
-}else if (nota >= 50){
-  console.log ('E')
-} else if (nota < 50){
-  console.log('F')
-}
-}else{
+if (nota < 0 || nota > 100) {
   console.log('SR sem rendimento ou nota invalidae!')
+  return;
+} else if (nota >= 90) {
+  console.log('A')
+} else if (nota >= 80) {
+  console.log('B')
+} else if (nota >= 70) {
+  console.log('C')
+} else if (nota >= 60) {
+  console.log('D')
+} else if (nota >= 50) {
+  console.log('E')
+} else if (nota < 50) {
+  console.log('F')
 }
