87f330b2659e79501a4c1449ff7cef99e1b702e7
walvesb65 <willian.alvesb65@gmail.com>
2021-02-28 02:49:17 -0300

    corrigindo a tividade 1 e 2

--- exercises/Bloco_4/Dia_2/script2.js
+++ exercises/Bloco_4/Dia_2/script2.js
@@ -1,4 +1,4 @@
-/* Aprofunde seus conhecimentosLeia atentamente os enunciados e faça o
+/* Aprofunde seus conhecimentos Leia atentamente os enunciados e faça o
  que se pede! Você irá utilizar esse array para realizar os exercícios do 1 ao 7:
 
 Questão 2 - Para o segundo exercício, você deve somar todos os valores contidos no array
@@ -7,8 +7,9 @@ Questão 2 - Para o segundo exercício, você deve somar todos os valores contid
  */
 
   let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-  let resultado = 0
-  for(let index = 0; index < numbers.length; index++){
-    resultado += numbers[index]
-    console.log(resultado)
+  let resultado = 0;
+  
+  for(let index = 0; index < numbers.length; index +=1){ 
+   resultado = numbers[index] + resultado
   }
+  console.log(resultado)
