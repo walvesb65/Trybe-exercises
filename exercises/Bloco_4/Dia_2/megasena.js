ec4668539c7acadbaa475ad18bf90fa01943d173
walvesb65 <willian.alvesb65@gmail.com>
2021-03-03 01:17:59 -0300

    add math.ceil e math.random

--- exercises/Bloco_4/Dia_2/megasena.js
+++ exercises/Bloco_4/Dia_2/megasena.js
@@ -1,10 +1,10 @@
 let megaSena = [];
 
-let primeiroNumero;
+let primeiroNumero = Math.ceil(Math.random() * 60); /* gera numero aleatorio ente 1 e 60 */
 let segundoNumero;
 let terceiroNumero;
 let quartoNumero;
 let quintoNumero;
 let sextoNumero;
 
-console.log(Math.random());
+console.log(primeiroNumero); /* impreme a variavel */
