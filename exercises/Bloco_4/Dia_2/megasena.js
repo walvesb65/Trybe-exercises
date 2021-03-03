463ae38ea1cfc7940280bfe2690b835035b208a2
walvesb65 <willian.alvesb65@gmail.com>
2021-03-03 01:25:31 -0300

    gera numeros da megasea

--- exercises/Bloco_4/Dia_2/megasena.js
+++ exercises/Bloco_4/Dia_2/megasena.js
@@ -1,10 +1,12 @@
 let megaSena = [];
 
 let primeiroNumero = Math.ceil(Math.random() * 60); /* gera numero aleatorio ente 1 e 60 */
-let segundoNumero;
-let terceiroNumero;
-let quartoNumero;
-let quintoNumero;
-let sextoNumero;
+let segundoNumero = Math.ceil(Math.random() * 60); 
+let terceiroNumero = Math.ceil(Math.random() * 60); 
+let quartoNumero = Math.ceil(Math.random() * 60); 
+let quintoNumero = Math.ceil(Math.random() * 60); 
+let sextoNumero = Math.ceil(Math.random() * 60); 
 
-console.log(primeiroNumero); /* impreme a variavel */
+megaSena = [primeiroNumero ,segundoNumero ,terceiroNumero, quartoNumero, quintoNumero, sextoNumero]
+
+console.log(megaSena);
