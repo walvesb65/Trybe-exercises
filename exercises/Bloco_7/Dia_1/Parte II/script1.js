775177d9c8e2517e58f645ce8602664f280552b9
walvesb65 <willian.alvesb65@gmail.com>
2021-03-27 00:21:24 -0300

    Atividade 3

---
+++ exercises/Bloco_7/Dia_1/Parte II/script1.js
@@ -0,0 +1,11 @@
+const buttonpage = document.getElementById('click');
+let  clickCount = 0;
+let divCount = document.querySelector('div');
+
+const clickpage = () => {
+buttonpage.addEventListener('click', () => {
+clickCount = clickCount + 1
+divCount.innerText = clickCount
+})
+}
+clickpage()
