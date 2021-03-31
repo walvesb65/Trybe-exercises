292ca6ad40f342408cde4c1b093a1471c71bfa6f
walvesb65 <willian.alvesb65@gmail.com>
2021-03-30 21:59:21 -0300

    questão 1 parte dois

---
+++ exercises/Bloco_7/Dia_2/Parte II/acript.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const novoTurno = (objeto, chave, valor) => {
+  objeto[chave] = valor
+  return objeto
+}
+console.log(novoTurno(lesson2, 'turno', 'manhã'));
