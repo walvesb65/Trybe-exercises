066a7c98f6007b09a3796a4212ef66fe553ea0d0
walvesb65 <willian.alvesb65@gmail.com>
2021-03-31 20:15:51 -0300

    questão 4

--- exercises/Bloco_7/Dia_3/teste3.js
+++ exercises/Bloco_7/Dia_3/teste3.js
@@ -22,3 +22,4 @@ function myRemoveWithoutCopy(arr, item) {
 
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4])
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4] )
