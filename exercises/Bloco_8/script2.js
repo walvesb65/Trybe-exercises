18fa2d1b014f6b1f152c0433b87dd03a09803959
walvesb65 <willian.alvesb65@gmail.com>
2021-04-06 12:29:48 -0300

    Atividade do bloco 8 dia 1 questão2

--- exercises/Bloco_8/script2.js
+++ exercises/Bloco_8/script2.js
@@ -1,1 +1,15 @@
 /* Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). */
+
+const resultadoSorteio = (numApost, funcao) => {
+  let numSort = Math.ceil(Math.random() * 5) 
+  return funcao(numSort, numApost);
+}
+
+const checkSorteio = (resultadoSorteio, numApost) => {
+  if(resultadoSorteio === numApost){
+    return 'Parabéns você ganhou'
+  } else {
+    return 'Tente novamente'
+  }
+}
+console.log(resultadoSorteio(1, checkSorteio));
